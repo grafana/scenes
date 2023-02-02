4c7bd9481578fcae3d5fb5bb1b8e5351340cdfce
Ivan Ortega <ivanortegaalba@gmail.com>
2023-02-02 16:28:00 +0100

    Fix test issues

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,10 @@
 const esModules = ['ol', 'd3', 'd3-color', 'd3-interpolate', 'delaunator', 'internmap', 'robust-predicates'].join('|');
 
 module.exports = {
+  moduleNameMapper: {
+    // D3 exposes ESModules. To avoid issues with Jest, we need to point them to the CJS version.
+    d3: '<rootDir>/node_modules/d3/dist/d3.min.js',
+  },
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./utils/setupTests.ts'],
   testMatch: ['<rootDir>/src/**/*.{spec,test,jest}.{js,jsx,ts,tsx}'],
