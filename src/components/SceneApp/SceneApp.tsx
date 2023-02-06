4a13ad5a593a1074d3e0eea5ec451040e62055ed
Dominik Prokop <dominik.prokop@grafana.com>
2023-02-06 15:28:00 +0100

    Init url sync before scene is actually activated.

--- src/components/SceneApp/SceneApp.tsx
+++ src/components/SceneApp/SceneApp.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 
 import { NavModelItem, UrlQueryMap } from '@grafana/data';
+import { PluginPage } from '@grafana/runtime';
 
 import { SceneComponentProps, SceneObject, SceneObjectStatePlain } from '../../core/types';
 import { SceneObjectBase } from '../../core/SceneObjectBase';
 import { getLinkUrlWithAppUrlState, useAppQueryParams } from './utils';
-import { PluginPage } from '@grafana/runtime';
 import { EmbeddedScene } from '../EmbeddedScene';
+
+const sceneCache = new Map<string, EmbeddedScene>();
 
 export interface SceneAppState extends SceneObjectStatePlain {
   pages: SceneAppPage[];
@@ -26,6 +28,9 @@ export interface SceneAppRoute {
   drilldown?: SceneAppDrilldownView;
 }
 
+/**
+ * Responsible for top level pages routing
+ */
 export class SceneApp extends SceneObjectBase<SceneAppState> {
   public static Component = ({ model }: SceneComponentProps<SceneApp>) => {
     const { pages } = model.useState();
@@ -33,9 +38,14 @@ export class SceneApp extends SceneObjectBase<SceneAppState> {
     return (
       <Switch>
         {pages.map((page) => (
-          <Route key={page.state.url} exact={false} path={page.state.url}>
-            {page && <page.Component model={page} />}
-          </Route>
+          <Route
+            key={page.state.url}
+            exact={false}
+            path={page.state.url}
+            render={() => {
+              return page && <page.Component model={page} />;
+            }}
+          ></Route>
         ))}
       </Switch>
     );
@@ -44,9 +54,9 @@ export class SceneApp extends SceneObjectBase<SceneAppState> {
 
 export interface SceneAppPageState extends SceneObjectStatePlain {
   title: string;
+  subTitle?: string;
   url: string;
   routePath?: string;
-  subTitle?: string;
   hideFromBreadcrumbs?: boolean;
   tabs?: SceneAppPage[];
   getScene: (routeMatch: SceneRouteMatch) => EmbeddedScene;
@@ -55,91 +65,126 @@ export interface SceneAppPageState extends SceneObjectStatePlain {
   preserveUrlKeys?: string[];
 }
 
-const sceneCache = new Map<string, EmbeddedScene>();
-
+/**
+ * Responsible for page's drilldown & tabs routing
+ */
 export class SceneAppPage extends SceneObjectBase<SceneAppPageState> {
-  public static Component = ({ model }: SceneComponentProps<SceneAppPage>) => {
-    const { tabs, drilldowns, url, routePath } = model.state;
+  public static Component = SceneAppPageRenderer;
+}
 
-    const routes: React.ReactNode[] = [];
+function SceneAppPageRenderer({ model }: SceneComponentProps<SceneAppPage>) {
+  const { tabs, drilldowns, url, routePath } = model.state;
+  const routes: React.ReactNode[] = [];
 
-    if (tabs) {
-      for (const page of tabs) {
-        routes.push(
-          <Route key={page.state.url} exact={true} path={page.state.routePath ?? page.state.url}>
-            <page.Component model={page} />
-          </Route>
-        );
+  console.log('page renderer', url);
+  if (tabs) {
+    for (const page of tabs) {
+      routes.push(
+        <Route
+          exact={true}
+          key={page.state.url}
+          path={page.state.routePath ?? page.state.url}
+          render={() => {
+            debugger;
+            return <page.Component model={page} />;
+          }}
+        ></Route>
+      );
 
-        if (page.state.drilldowns) {
-          for (const drilldown of page.state.drilldowns) {
-            console.log('registering drilldown route', drilldown.routePath);
-            routes.push(
-              <Route key={drilldown.routePath} exact={false} path={drilldown.routePath}>
-                <SceneAppDrilldownViewRender drilldown={drilldown} parent={page} />
-              </Route>
-            );
-          }
+      if (page.state.drilldowns) {
+        for (const drilldown of page.state.drilldowns) {
+          routes.push(
+            <Route
+              exact={false}
+              key={drilldown.routePath}
+              path={drilldown.routePath}
+              render={() => {
+                return <SceneAppDrilldownViewRender drilldown={drilldown} parent={page} />;
+              }}
+            ></Route>
+          );
         }
       }
-
-      return <Switch>{routes}</Switch>;
     }
 
-    if (drilldowns) {
-      for (const drilldown of drilldowns) {
-        console.log('registering non-tab drilldown route', drilldown.routePath);
-        routes.push(
-          <Route key={drilldown.routePath} exact={false} path={drilldown.routePath}>
-            <SceneAppDrilldownViewRender drilldown={drilldown} parent={model} />
-          </Route>
-        );
-      }
+    console.log('routes', routes);
+    return <Switch>{routes}</Switch>;
+  }
+
+  if (drilldowns) {
+    for (const drilldown of drilldowns) {
+      routes.push(
+        <Route
+          key={drilldown.routePath}
+          exact={false}
+          path={drilldown.routePath}
+          render={() => {
+            return <SceneAppDrilldownViewRender drilldown={drilldown} parent={model} />;
+          }}
+        ></Route>
+      );
     }
+  }
 
-    const routeMatch = useRouteMatch();
-    console.log('routeMatch path', routeMatch.url);
+  let page = <ScenePageRenderer page={model} />;
 
-    let scene = sceneCache.get(routeMatch.url);
-    if (!scene) {
-      scene = model.state.getScene(routeMatch);
-      sceneCache.set(routeMatch.url, scene);
-    }
+  // if parent is a SceneAppPage we are a tab
+  if (model.parent instanceof SceneAppPage) {
+    page = <ScenePageRenderer page={model.parent} activeTab={model} tabs={model.parent.state.tabs} />;
+  }
 
-    scene.initUrlSync();
+  return (
+    <Switch>
+      {/* page route */}
+      <Route
+        key={url}
+        exact={true}
+        path={routePath ?? url}
+        render={() => {
+          return page;
+        }}
+      ></Route>
 
-    console.log('rendering page!', model.state.url);
+      {/* drilldown routes */}
+      {routes}
+    </Switch>
+  );
 
-    let page = <PageRenderer page={model} scene={scene} />;
-    // if parent is a SceneAppPage we are a tab
-    if (model.parent instanceof SceneAppPage) {
-      page = <PageRenderer page={model.parent} scene={scene} activeTab={model} tabs={model.parent.state.tabs} />;
-    }
-
-    return (
-      <Switch>
-        {/* page route */}
-        <Route key={url} exact={true} path={routePath ?? url}>
-          {page}
-        </Route>
-        {/* drilldown routes */}
-        {routes}
-      </Switch>
-    );
-
-    // return ;
-  };
+  // return ;
 }
 
 interface ScenePageRenderProps {
   page: SceneAppPage;
   tabs?: SceneAppPage[];
   activeTab?: SceneAppPage;
-  scene: SceneObject;
 }
 
-function PageRenderer({ page, tabs, activeTab, scene }: ScenePageRenderProps) {
+function ScenePageRenderer({ page, tabs, activeTab }: ScenePageRenderProps) {
+  /**
+   * We use this flag to make sure the URL sync is enabled before the scene is actually rendered.
+   */
+  const [isInitialized, setIsInitialized] = useState(false);
   const params = useAppQueryParams();
+  const routeMatch = useRouteMatch();
+
+  let scene = sceneCache.get(routeMatch!.url);
+  if (!scene) {
+    // If we are rendering a tab, we need to get the scene f  rom the tab, otherwise, use page's scene
+    scene = activeTab ? activeTab.state.getScene(routeMatch) : page.state.getScene(routeMatch);
+    sceneCache.set(routeMatch!.url, scene);
+  }
+
+  useEffect(() => {
+    // Before rendering scene components, we are making sure the URL sync is enabled for.
+    if (!isInitialized && scene) {
+      scene.initUrlSync();
+      setIsInitialized(true);
+    }
+  }, [isInitialized, scene]);
+
+  if (!isInitialized) {
+    return null;
+  }
 
   const pageNav: NavModelItem = {
     text: page.state.title,
@@ -152,10 +197,10 @@ function PageRenderer({ page, tabs, activeTab, scene }: ScenePageRenderProps) {
   if (tabs) {
     pageNav.children = tabs.map((tab) => {
       return {
-      text: tab.state.title,
-      active: activeTab === tab,
+        text: tab.state.title,
+        active: activeTab === tab,
         url: getLinkUrlWithAppUrlState(tab.state.url, params, tab.state.preserveUrlKeys),
-      parentItem: pageNav,
+        parentItem: pageNav,
       };
     });
   }
@@ -165,6 +210,17 @@ function PageRenderer({ page, tabs, activeTab, scene }: ScenePageRenderProps) {
       <scene.Component model={scene} />
     </PluginPage>
   );
+}
+
+export interface SceneAppDrilldownView {
+  routePath: string;
+  getPage: (routeMatch: SceneRouteMatch<any>, parent: SceneAppPage) => SceneAppPage;
+}
+
+export function SceneAppDrilldownViewRender(props: { drilldown: SceneAppDrilldownView; parent: SceneAppPage }) {
+  const routeMatch = useRouteMatch();
+  const scene = props.drilldown.getPage(routeMatch, props.parent);
+  return <scene.Component model={scene} />;
 }
 
 function getParentBreadcrumbs(parent: SceneObject | undefined, params: UrlQueryMap): NavModelItem | undefined {
@@ -181,16 +237,4 @@ function getParentBreadcrumbs(parent: SceneObject | undefined, params: UrlQueryM
   }
 
   return undefined;
-}
-
-export interface SceneAppDrilldownView {
-  routePath: string;
-  getPage: (routeMatch: SceneRouteMatch<any>, parent: SceneAppPage) => SceneAppPage;
-}
-
-export function SceneAppDrilldownViewRender(props: { drilldown: SceneAppDrilldownView; parent: SceneAppPage }) {
-  const routeMatch = useRouteMatch();
-  const scene = props.drilldown.getPage(routeMatch, props.parent);
-  console.log('drilldown!');
-  return <scene.Component model={scene} />;
 }
