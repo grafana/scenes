080a1d6d84acb7bc262e30bb0acba4a93277001c
Torkel Ödegaard <torkel@grafana.com>
2023-02-15 09:45:12 +0100

    Update

--- src/variables/VariableDependencyConfig.test.ts
+++ src/variables/VariableDependencyConfig.test.ts
@@ -1,4 +1,3 @@
-import { SceneFlexLayout, SceneVariableSet, TestVariable } from '@grafana/scenes';
 import { SceneObjectBase } from '../core/SceneObjectBase';
 import { SceneObjectStatePlain } from '../core/types';
 
