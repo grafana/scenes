cdc9ee561ff89cd959e278d620fdda738aeed9ed
Dominik Prokop <dominik.prokop@grafana.com>
2023-03-15 11:12:19 +0100

    Monorepo setup with correct publish script (#62)
    * Revert "Revert "Monorepo setup (#52)" (#61)"
    
    This reverts commit 67152d6b8d331f3a32f23caf723ce00d9a37606f.
    
    * Update publish script

---
+++ packages/scenes-app/src/components/Routes/Routes.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { HomePage } from '../../pages/Home';
+import { PageWithTabs } from '../../pages/WithTabs';
+import { WithDrilldown } from '../../pages/WithDrilldown';
+import { prefixRoute } from '../../utils/utils.routing';
+import { ROUTES } from '../../constants';
+
+export const Routes = () => {
+  return (
+    <Switch>
+      <Route path={prefixRoute(`${ROUTES.WithTabs}`)} component={PageWithTabs} />
+      <Route path={prefixRoute(`${ROUTES.WithDrilldown}`)} component={WithDrilldown} />
+
+      {/* Full-width page (this page will have no navigation bar) */}
+
+      {/* Default page */}
+      <Route path={prefixRoute(`${ROUTES.Home}`)} component={HomePage} />
+
+      <Redirect to={prefixRoute(ROUTES.Home)} />
+    </Switch>
+  );
+};
