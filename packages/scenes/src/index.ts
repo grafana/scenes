dcaba878eb042ad4242a0a8870a24ae210323d49
Torkel Ödegaard <torkel@grafana.com>
2023-03-16 07:13:27 +0100

    Updates

--- packages/scenes/src/index.ts
+++ packages/scenes/src/index.ts
@@ -45,3 +45,4 @@ export {
 } from './components/SceneApp/types';
 export { SceneApp } from './components/SceneApp/SceneApp';
 export { SceneAppPage } from './components/SceneApp/SceneAppPage';
+export { SceneControlWrapper } from './components/SceneControlWrapper';
