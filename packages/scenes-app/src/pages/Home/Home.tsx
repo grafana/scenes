40819c015865d9a5768eefe72eadd2a36c37894c
Ryan McKinley <ryantxu@gmail.com>
2023-03-29 09:15:36 -0700

    setup warnings

--- packages/scenes-app/src/pages/Home/Home.tsx
+++ packages/scenes-app/src/pages/Home/Home.tsx
@@ -2,7 +2,9 @@ import { SceneApp, SceneAppPage } from '@grafana/scenes';
 import * as React from 'react';
 import { getBasicScene } from './scenes';
 import { prefixRoute } from '../../utils/utils.routing';
-import { ROUTES } from '../../constants';
+import { DATASOURCE_REF, ROUTES } from '../../constants';
+import { config } from '@grafana/runtime';
+import { Alert } from '@grafana/ui';
 
 const getScene = () => {
   return new SceneApp({
@@ -21,5 +23,17 @@ const getScene = () => {
 };
 export const HomePage = () => {
   const scene = getScene();
-  return <scene.Component model={scene} />;
+
+  return <>
+    {!config.featureToggles.topnav && <Alert title='Missing topnav feature toggle'>
+      Scenes are designed to work with the new navigation wrapper that will be standard in grafana 10
+    </Alert>}
+
+    {!config.datasources[DATASOURCE_REF.uid] && <Alert title={`Missing ${DATASOURCE_REF.uid} datasource`}>
+      These demos depend on <b>testdata</b> datasource: <code>{JSON.stringify(DATASOURCE_REF)}</code>.  
+      This can be provisioned using the attached datasource provisioning files.
+    </Alert>}
+
+    <scene.Component model={scene} />
+  </>;
 };
