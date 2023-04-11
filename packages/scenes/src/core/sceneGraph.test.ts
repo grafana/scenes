1ee4eaa99d095c6cc8abcc893c9ca04a5edf0376
Torkel Ödegaard <torkel@grafana.com>
2023-04-11 08:05:55 +0200

    FindObject: Fixes search logic so that it does not get stuck in infinite loops

--- packages/scenes/src/core/sceneGraph.test.ts
+++ packages/scenes/src/core/sceneGraph.test.ts
@@ -2,15 +2,18 @@ import { EmbeddedScene } from '../components/EmbeddedScene';
 import { SceneFlexItem, SceneFlexLayout } from '../components/layout/SceneFlexLayout';
 import { SceneCanvasText } from '../components/SceneCanvasText';
 import { SceneTimePicker } from '../components/SceneTimePicker';
+import { SceneDataNode } from './SceneDataNode';
 import { sceneGraph } from './sceneGraph';
 
 describe('sceneGraph', () => {
   it('Can find object', () => {
-    const item1 = new SceneFlexItem({ key: 'A', body: new SceneCanvasText({ text: 'A' }) });
+    const data = new SceneDataNode();
+    const item1 = new SceneFlexItem({ key: 'A', body: new SceneCanvasText({ text: 'A' }), $data: data });
     const item2 = new SceneFlexItem({ key: 'B', body: new SceneCanvasText({ text: 'B' }) });
+    const timePicker = new SceneTimePicker({ key: 'time-picker' });
 
     const scene = new EmbeddedScene({
-      controls: [new SceneTimePicker({})],
+      controls: [timePicker],
       body: new SceneFlexLayout({
         children: [item1, item2],
       }),
@@ -20,5 +23,9 @@ describe('sceneGraph', () => {
     expect(sceneGraph.findObject(scene, (s) => s.state.key === 'A')).toBe(item1);
     // from sibling
     expect(sceneGraph.findObject(item2, (s) => s.state.key === 'A')).toBe(item1);
+    // from data
+    expect(sceneGraph.findObject(data, (s) => s.state.key === 'A')).toBe(item1);
+    // from item deep in graph finding control
+    expect(sceneGraph.findObject(item2, (s) => s.state.key === 'time-picker')).toBe(timePicker);
   });
 });
