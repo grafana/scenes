2777ee8116030620657cfa6571c8ece945df648c
Dominik Prokop <dominik.prokop@grafana.com>
2023-05-11 15:55:25 +0200

    Docs: Make sidebar expanded by default

--- docusaurus/website/docusaurus.config.js
+++ docusaurus/website/docusaurus.config.js
@@ -53,6 +53,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      docs: {
+        sidebar: {
+          autoCollapseCategories: false,
+        },
+      },
       navbar: {
         title: 'Grafana Scenes',
         logo: {
