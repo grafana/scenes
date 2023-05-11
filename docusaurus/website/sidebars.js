e3a26e972a387377ea01ad77480cd8cd783790bf
Dominik Prokop <dominik.prokop@grafana.com>
2023-05-11 16:17:12 +0200

    Docs: Make sidebar expanded by default (#183)

--- docusaurus/website/sidebars.js
+++ docusaurus/website/sidebars.js
@@ -13,32 +13,32 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  docs: {
-    'Getting started': [{ id: 'getting-started', label: 'Setup', type: 'doc' }, 'core-concepts'],
-    'Scene objects': [
-      { id: 'scene-layout', label: 'Layouts', type: 'doc' },
-      'visualizations',
-      'variables',
-      'transformations',
-      {
-        type: 'category',
-        label: 'Scene apps',
-        collapsible: true,
-        collapsed: false,
-        items: [
-          'scene-app',
-          { id: 'scene-app-tabs', label: 'Tabs', type: 'doc' },
-          { id: 'scene-app-drilldown', label: 'Drill-down pages', type: 'doc' },
-        ],
-      },
-    ],
-    'Advanced usage': [
-      'advanced-custom-scene-objects',
-      'advanced-activation-handlers',
-      'advanced-data',
-      'advanced-variables',
-    ],
-  },
+  sidebar: [
+    {
+      type: 'category',
+      label: 'Getting started',
+      collapsible: true,
+      collapsed: false,
+      items: [{ id: 'getting-started', label: 'Setup', type: 'doc' }, 'core-concepts'],
+    },
+    {
+      type: 'category',
+      label: 'Scene apps',
+      collapsible: true,
+      collapsed: false,
+      items: [
+        'scene-app',
+        { id: 'scene-app-tabs', label: 'Tabs', type: 'doc' },
+        { id: 'scene-app-drilldown', label: 'Drill-down pages', type: 'doc' },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Advanced usage',
+      collapsible: true,
+      collapsed: false,
+      items: ['advanced-custom-scene-objects', 'advanced-activation-handlers', 'advanced-data', 'advanced-variables'],
+    },
+  ],
 };
-
 module.exports = sidebars;
