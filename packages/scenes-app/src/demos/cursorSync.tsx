1446efa65488d8b6e4d84b7fd8a1f0629e8d235c
Dominik Prokop <dominik.prokop@grafana.com>
2023-07-13 16:14:53 +0200

    Demo update

--- packages/scenes-app/src/demos/cursorSync.tsx
+++ packages/scenes-app/src/demos/cursorSync.tsx
@@ -9,12 +9,96 @@ import {
   VizPanel,
 } from '@grafana/scenes';
 import { DashboardCursorSync } from '@grafana/schema';
+import { demoUrl } from '../utils/utils.routing';
 import { getEmbeddedSceneDefaults, getQueryRunnerWithRandomWalkQuery } from './utils';
 
-export function getCursorSyncTest(defaults: SceneAppPageState) {
+export function global() {
   return new SceneAppPage({
-    ...defaults,
-    subTitle: 'A simple demo of scoped cursor sync',
+    title: 'Cursor sync test',
+    url: demoUrl('cursor-sync'),
+    getScene: () => {
+      return new EmbeddedScene({
+        ...getEmbeddedSceneDefaults(),
+        key: 'Flex layout embedded scene',
+        $behaviors: [new behaviors.CursorSync({ key: 'sync1', sync: DashboardCursorSync.Tooltip })],
+        body: new SceneFlexLayout({
+          direction: 'column',
+          children: [
+            new SceneFlexLayout({
+              direction: 'row',
+              children: [
+                new SceneFlexItem({
+                  body: new VizPanel({
+                    title: 'Panel 1 - syncs tooltip with Panel 2',
+                    pluginId: 'timeseries',
+                    $data: getQueryRunnerWithRandomWalkQuery({
+                      scenarioId: 'csv_metric_values',
+                      stringInput: '1,20,90,30,5,0',
+                    }),
+                  }),
+                }),
+                new SceneFlexItem({
+                  body: PanelBuilders.timeseries()
+                    .setData(
+                      getQueryRunnerWithRandomWalkQuery(
+                        {
+                          scenarioId: 'csv_metric_values',
+                          stringInput: '1,20,90,30,5,0',
+                        },
+                        { maxDataPoints: 50 }
+                      )
+                    )
+                    .setTitle('Panel 2')
+                    .build(),
+                }),
+              ],
+            }),
+            new SceneFlexLayout({
+              direction: 'row',
+              children: [
+                new SceneFlexItem({
+                  body: new VizPanel({
+                    title: 'Panel 3 - syncs crosshair with Panel 4',
+                    pluginId: 'timeseries',
+                    $data: getQueryRunnerWithRandomWalkQuery({}),
+                  }),
+                }),
+                new SceneFlexItem({
+                  body: PanelBuilders.timeseries()
+                    .setData(getQueryRunnerWithRandomWalkQuery({}, { maxDataPoints: 50 }))
+                    .setTitle('Panel 4')
+                    .build(),
+                }),
+              ],
+            }),
+            new SceneFlexLayout({
+              direction: 'row',
+              children: [
+                new SceneFlexItem({
+                  body: new VizPanel({
+                    title: 'No sync',
+                    pluginId: 'timeseries',
+                    $data: getQueryRunnerWithRandomWalkQuery({}),
+                  }),
+                }),
+                new SceneFlexItem({
+                  body: PanelBuilders.timeseries()
+                    .setData(getQueryRunnerWithRandomWalkQuery({}, { maxDataPoints: 50 }))
+                    .setTitle('No sync')
+                    .build(),
+                }),
+              ],
+            }),
+          ],
+        }),
+      });
+    },
+  });
+}
+export function scoped() {
+  return new SceneAppPage({
+    title: 'Cursor sync test',
+    url: demoUrl('cursor-sync/scoped'),
     getScene: () => {
       return new EmbeddedScene({
         ...getEmbeddedSceneDefaults(),
@@ -126,5 +210,14 @@ export function getCursorSyncTest(defaults: SceneAppPageState) {
         }),
       });
     },
+  });
+}
+
+export function getCursorSyncTest(defaults: SceneAppPageState) {
+  return new SceneAppPage({
+    ...defaults,
+    subTitle: 'A simple demo of scoped cursor sync',
+    url: demoUrl('cursor-sync'),
+    tabs: [global(), scoped()],
   });
 }
