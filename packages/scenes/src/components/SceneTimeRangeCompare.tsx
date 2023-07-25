ce294d4e863d144bc162fd4e5e5567328183b19a
kay delaney <kay@grafana.com>
2023-07-25 15:09:16 +0100

    Generate sensible defaults for compare options based on current time range

--- packages/scenes/src/components/SceneTimeRangeCompare.tsx
+++ packages/scenes/src/components/SceneTimeRangeCompare.tsx
@@ -3,6 +3,7 @@ import { dateTime, rangeUtil, TimeRange } from '@grafana/data';
 import { ButtonSelect, IconButton, InlineField } from '@grafana/ui';
 import { SceneObjectBase } from '../core/SceneObjectBase';
 import { SceneComponentProps, SceneObjectState } from '../core/types';
+import { sceneGraph } from '../core/sceneGraph';
 
 export interface TimeRangeCompareProvider {
   getCompareTimeRange(timeRange: TimeRange): TimeRange | undefined;
@@ -11,6 +12,18 @@ export interface TimeRangeCompareProvider {
 interface SceneTimeRangeCompareState extends SceneObjectState {
   compareWith?: string;
 }
+
+const DEFAULT_COMPARE_OPTIONS = [
+  { label: 'A day ago', days: 1, value: '24h' },
+  { label: '3 days ago', days: 3, value: '3d', },
+  { label: 'A week ago', days: 7, value: '1w', },
+  { label: '2 weeks ago', days: 14, value: '2w', },
+  { label: 'A month ago', days: 31, value: '1M', },
+  { label: '6 months ago', days: 31*6, value: '6M', },
+  { label: 'A year ago', days: 365, value: '1y' }
+];
+
+const ONE_DAY_MS_LENGTH = 1000 * 60 * 60 * 24;
 
 export class SceneTimeRangeCompare
   extends SceneObjectBase<SceneTimeRangeCompareState>
@@ -23,19 +36,11 @@ export class SceneTimeRangeCompare
   }
 
   public getCompareOptions = () => {
-    // const timeRange = sceneGraph.getTimeRange(this);
+    const timeRange = sceneGraph.getTimeRange(this);
+    const diffDays = Math.ceil(timeRange.state.value.to.diff(timeRange.state.value.from) / ONE_DAY_MS_LENGTH);
+    const matchIndex = DEFAULT_COMPARE_OPTIONS.findIndex(({ days }) => days >= diffDays);
 
-    // TODO - those options should be provided based on the selected time range, this is faking for now
-    return [
-      {
-        label: 'A day ago',
-        value: '24h',
-      },
-      {
-        label: 'A week ago',
-        value: '7d',
-      },
-    ];
+    return DEFAULT_COMPARE_OPTIONS.slice(matchIndex).map(({ label, value }) => ({ label, value }));
   };
 
   public onCompareWithChanged = (compareWith: string) => {
