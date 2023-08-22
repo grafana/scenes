b88a03b802372f507411103bc24aac039fead691
kay delaney <45561153+kaydelaney@users.noreply.github.com>
2023-08-22 15:22:07 +0100

    Allow template variables to be cancelled (#261)

--- packages/scenes/src/variables/components/VariableValueSelect.tsx
+++ packages/scenes/src/variables/components/VariableValueSelect.tsx
@@ -1,16 +1,45 @@
 import { isArray } from 'lodash';
-import React from 'react';
+import React, { useCallback } from 'react';
 
-import { MultiSelect, Select } from '@grafana/ui';
+import { Icon, MultiSelect, Select, Tooltip } from '@grafana/ui';
+import { LoadingIndicatorProps } from 'react-select';
 
 import { SceneComponentProps } from '../../core/types';
 import { MultiValueVariable } from '../variants/MultiValueVariable';
+import { selectors } from '@grafana/e2e-selectors';
+import { VariableValue, VariableValueSingle } from '../types';
+
+
+const LoadingIndicator = ({ innerProps, ...props }: LoadingIndicatorProps & { selectProps: { onCancel: () => void }}) => {
+  const { onCancel } = props.selectProps;
+  const onMouseDown = useCallback(
+    (event: React.MouseEvent) => {
+      event.preventDefault();
+      event.stopPropagation();
+      onCancel();
+    },
+    [onCancel]
+  );
+
+  return (
+    <Tooltip content="Cancel query">
+      <Icon
+        className="spin-clockwise"
+        name="sync"
+        size="xs"
+        aria-label={selectors.components.LoadingIndicator.icon}
+        role="button"
+        onMouseDown={onMouseDown}
+      />
+    </Tooltip>
+  );
+};
 
 export function VariableValueSelect({ model }: SceneComponentProps<MultiValueVariable>) {
   const { value, key, loading } = model.useState();
 
   return (
-    <Select
+    <Select<VariableValue, { onCancel: () => void }>
       id={key}
       placeholder="Select value"
       width="auto"
@@ -22,6 +51,8 @@ export function VariableValueSelect({ model }: SceneComponentProps<MultiValueVar
       onChange={(newValue) => {
         model.changeValueTo(newValue.value!, newValue.label!);
       }}
+      onCancel={() => { model.cancel?.() }}
+      components={{ LoadingIndicator }}
     />
   );
 }
@@ -31,7 +62,7 @@ export function VariableValueSelectMulti({ model }: SceneComponentProps<MultiVal
   const arrayValue = isArray(value) ? value : [value];
 
   return (
-    <MultiSelect
+    <MultiSelect<VariableValueSingle, { onCancel: () => void }>
       id={key}
       placeholder="Select value"
       width="auto"
@@ -43,6 +74,8 @@ export function VariableValueSelectMulti({ model }: SceneComponentProps<MultiVal
       closeMenuOnSelect={false}
       isClearable={true}
       onOpenMenu={() => {}}
+      onCancel={() => { model.cancel?.() }}
+      components={{ LoadingIndicator }}
       onChange={(newValue) => {
         model.changeValueTo(
           newValue.map((v) => v.value!),
