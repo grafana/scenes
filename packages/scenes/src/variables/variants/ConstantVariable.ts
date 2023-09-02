8956124ac7c54ca9674ad9fe6acf037228198191
Torkel Ödegaard <torkel@grafana.com>
2023-09-02 14:41:01 +0200

    Add text representation to ConstantVariable

--- packages/scenes/src/variables/variants/ConstantVariable.ts
+++ packages/scenes/src/variables/variants/ConstantVariable.ts
@@ -3,6 +3,7 @@ import { SceneVariable, SceneVariableState, VariableValue } from '../types';
 
 export interface ConstantVariableState extends SceneVariableState {
   value: VariableValue;
+  text?: string;
 }
 
 export class ConstantVariable
@@ -21,5 +22,9 @@ export class ConstantVariable
 
   public getValue(): VariableValue {
     return this.state.value;
+  }
+
+  public getValueText(): string {
+    return this.state.text || this.state.value.toString();
   }
 }
