c9472c114099f7c38ee21c4396232e485965155c
Torkel Ödegaard <torkel@grafana.com>
2023-09-25 09:01:29 +0200

    Update

--- packages/scenes/src/variables/adhoc/AdHocFiltersVariable.test.tsx
+++ packages/scenes/src/variables/adhoc/AdHocFiltersVariable.test.tsx
@@ -1,4 +1,4 @@
-import { act, getAllByRole, render, screen, waitFor } from '@testing-library/react';
+import { getAllByRole, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { select } from 'react-select-event';
