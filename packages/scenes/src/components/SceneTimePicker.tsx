3102df5efa3ff1e5522928cbd1f2b59857ddb938
Polina Boneva <13227501+polibb@users.noreply.github.com>
2023-10-03 18:11:04 +0300

    SceneTimeRange: Implement onZoom behavior (#374)

--- packages/scenes/src/components/SceneTimePicker.tsx
+++ packages/scenes/src/components/SceneTimePicker.tsx
@@ -5,6 +5,7 @@ import { TimeRangePicker } from '@grafana/ui';
 import { SceneObjectBase } from '../core/SceneObjectBase';
 import { sceneGraph } from '../core/sceneGraph';
 import { SceneComponentProps, SceneObjectState } from '../core/types';
+import { TimeRange, toUtc } from '@grafana/data';
 
 export interface SceneTimePickerState extends SceneObjectState {
   hidePicker?: boolean;
@@ -13,6 +14,12 @@ export interface SceneTimePickerState extends SceneObjectState {
 
 export class SceneTimePicker extends SceneObjectBase<SceneTimePickerState> {
   public static Component = SceneTimePickerRenderer;
+
+  public onZoom = () => {
+    const timeRange = sceneGraph.getTimeRange(this);
+    const zoomedTimeRange = getZoomedTimeRange(timeRange.state.value, 2);
+    timeRange.onTimeRangeChange(zoomedTimeRange);
+  };
 
   public onChangeFiscalYearStartMonth = (month: number) => {
     const timeRange = sceneGraph.getTimeRange(this);
@@ -39,9 +46,21 @@ function SceneTimePickerRenderer({ model }: SceneComponentProps<SceneTimePicker>
       fiscalYearStartMonth={timeRangeState.fiscalYearStartMonth}
       onMoveBackward={() => {}}
       onMoveForward={() => {}}
-      onZoom={() => {}}
+      onZoom={model.onZoom}
       onChangeTimeZone={timeRange.onTimeZoneChange}
       onChangeFiscalYearStartMonth={model.onChangeFiscalYearStartMonth}
     />
   );
+}
+
+export function getZoomedTimeRange(timeRange: TimeRange, factor: number): TimeRange {
+  const timespan = timeRange.to.valueOf() - timeRange.from.valueOf();
+  const center = timeRange.to.valueOf() - timespan / 2;
+  // If the timepsan is 0, zooming out would do nothing, so we force a zoom out to 30s
+  const newTimespan = timespan === 0 ? 30000 : timespan * factor;
+
+  const to = center + newTimespan / 2;
+  const from = center - newTimespan / 2;
+
+  return { from: toUtc(from), to: toUtc(to), raw: { from: toUtc(from), to: toUtc(to) } };
 }
