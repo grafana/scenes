4ed682ddd06d86b52b0c24c370efa731dc81777f
Torkel Ödegaard <torkel@grafana.com>
2023-10-04 10:59:26 +0200

    minor comment fix

--- packages/scenes/src/utils/compatibility/setWindowGrafanaSceneContext.ts
+++ packages/scenes/src/utils/compatibility/setWindowGrafanaSceneContext.ts
@@ -2,8 +2,10 @@ import { SceneObject } from '../../core/types';
 import { writeSceneLog } from '../writeSceneLog';
 
 /**
- * Adds the scene object to the global state window state so that templateSrv in core can interpolate strings using the scene interpolation engine with the correct SceneObject scope.
- * This is needed for old data sources that call templateSrv.replace without passing scopedVars. For example in DataSourceAPI.metricFindQuery.
+ * Adds the scene object to the global window state so that templateSrv in core can interpolate strings using the scene interpolation engine with the scene as scope.
+ * This is needed for old datasources that call templateSrv.replace without passing scopedVars. For example in DataSourceAPI.metricFindQuery.
+ *
+ * This is also used from TimeSrv to access scene time range.
  */
 export function setWindowGrafanaSceneContext(activeScene: SceneObject) {
   const prevScene = (window as any).__grafanaSceneContext;
