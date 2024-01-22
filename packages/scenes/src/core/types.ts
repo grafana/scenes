b827025eee3025d6ab5868ee68c12dad84fd8768
Torkel Ödegaard <torkel@grafana.com>
2024-01-22 09:17:58 +0100

    Variables: Notify scene after each variable completion or value change  (#525)

--- packages/scenes/src/core/types.ts
+++ packages/scenes/src/core/types.ts
@@ -216,9 +216,6 @@ export interface SceneDataLayerProviderState extends SceneObjectState {
   data?: PanelData;
   isEnabled?: boolean;
   isHidden?: boolean;
-
-  // Private runtime state
-  _isWaitingForVariables?: boolean;
 }
 
 export interface SceneDataLayerProvider extends SceneObject<SceneDataLayerProviderState> {
