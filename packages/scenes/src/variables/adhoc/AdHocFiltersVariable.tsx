cb8bc06000f79458153f860358976eae30d3fb38
Javier Ruiz Calle <javier.ruizcalle@grafana.com>
2024-02-01 12:25:03 +0100

    Publish event on load, they might've changed due to url sync

--- packages/scenes/src/variables/adhoc/AdHocFiltersVariable.tsx
+++ packages/scenes/src/variables/adhoc/AdHocFiltersVariable.tsx
@@ -66,7 +66,7 @@ export class AdHocFiltersVariable
         })
       );
 
-      this._updateFilterExpression(this.state.set.state.filters, false);
+      this._updateFilterExpression(this.state.set.state.filters, true);
     });
   }
 
