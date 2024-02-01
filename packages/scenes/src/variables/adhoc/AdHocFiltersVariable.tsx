8fb0c5f9bdee4be2c4939122adb20f4bc05d8f86
Javier Ruiz Calle <javier.ruizcalle@grafana.com>
2024-02-01 14:30:07 +0100

    Undo notifying variable change on mount

--- packages/scenes/src/variables/adhoc/AdHocFiltersVariable.tsx
+++ packages/scenes/src/variables/adhoc/AdHocFiltersVariable.tsx
@@ -67,7 +67,7 @@ export class AdHocFiltersVariable
         })
       );
 
-      this._updateFilterExpression(this.state.set.state.filters, true);
+      this._updateFilterExpression(this.state.set.state.filters, false);
     });
   }
 
