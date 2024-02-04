ba1f4d470659c8bc0785e15ff98c5db16e5be78e
Torkel Ödegaard <torkel@grafana.com>
2024-02-04 18:34:55 +0100

    Fix grid demo

--- packages/scenes-app/src/demos/grid.tsx
+++ packages/scenes-app/src/demos/grid.tsx
@@ -1,11 +1,14 @@
 import {
   SceneGridLayout,
   SceneGridItem,
+  SceneFlexLayout,
+  SceneFlexItem,
   SceneAppPage,
   EmbeddedScene,
   SceneAppPageState,
   PanelBuilders,
 } from '@grafana/scenes';
+import { GRID_CELL_HEIGHT } from '@grafana/scenes/src/components/layout/grid/constants';
 import { getQueryRunnerWithRandomWalkQuery, getEmbeddedSceneDefaults } from './utils';
 
 export function getGridLayoutTest(defaults: SceneAppPageState): SceneAppPage {
@@ -38,6 +41,27 @@ export function getGridLayoutTest(defaults: SceneAppPageState): SceneAppPage {
               isResizable: false,
               isDraggable: false,
               body: PanelBuilders.timeseries().setTitle('No drag and no resize').build(),
+            }),
+            new SceneGridItem({
+              x: 0,
+              y: 11,
+              width: 24,
+              height: 10,
+              isDraggable: false,
+              isResizable: true,
+              body: new SceneFlexLayout({
+                direction: 'column',
+                // Auto 100% height for SceneFlexLayout inside grid is not working, need to make grid item display: flex
+                height: 10 * GRID_CELL_HEIGHT,
+                children: [
+                  new SceneFlexItem({
+                    body: PanelBuilders.timeseries().setTitle('Child of flex layout').build(),
+                  }),
+                  new SceneFlexItem({
+                    body: PanelBuilders.timeseries().setTitle('Child of flex layout').build(),
+                  }),
+                ],
+              }),
             }),
           ],
         }),
