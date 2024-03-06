dc5139bd19145425261812fa94d41418c24cb4aa
Torkel Ödegaard <torkel@grafana.com>
2024-03-06 12:58:08 +0100

    VizPanel: Fixes issue updating field config (#636)

--- packages/scenes/src/components/VizPanel/VizPanel.tsx
+++ packages/scenes/src/components/VizPanel/VizPanel.tsx
@@ -265,9 +265,8 @@ export class VizPanel<TOptions = {}, TFieldConfig extends {} = {}> extends Scene
       isAfterPluginChange: false,
     });
 
-    this.setState({
-      fieldConfig: withDefaults.fieldConfig,
-    });
+    this._dataWithFieldConfig = undefined;
+    this.setState({ fieldConfig: withDefaults.fieldConfig });
   };
 
   public interpolate = ((value: string, scoped?: ScopedVars, format?: string | VariableCustomFormatterFn) => {
@@ -407,10 +406,13 @@ export class VizPanel<TOptions = {}, TFieldConfig extends {} = {}> extends Scene
       sortBy = sortKey;
     }
 
-    this.onOptionsChange({
-      ...this.state.options,
-      legend: { ...legendOptions, sortBy, sortDesc },
-    } as TOptions, true);
+    this.onOptionsChange(
+      {
+        ...this.state.options,
+        legend: { ...legendOptions, sortBy, sortDesc },
+      } as TOptions,
+      true
+    );
   };
 
   private buildPanelContext(): PanelContext {
