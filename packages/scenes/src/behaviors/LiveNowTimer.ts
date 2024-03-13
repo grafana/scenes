d24f91b308898b46150071ca7c48dfc2178a1f80
kay delaney <45561153+kaydelaney@users.noreply.github.com>
2024-03-13 16:18:44 +0000

    LiveNow: Move enabled property to state (#642)

--- packages/scenes/src/behaviors/LiveNowTimer.ts
+++ packages/scenes/src/behaviors/LiveNowTimer.ts
@@ -3,17 +3,21 @@ import { SceneObjectBase } from "../core/SceneObjectBase";
 import { sceneGraph } from "../core/sceneGraph";
 import { SceneObjectState } from "../core/types";
 
-export class LiveNowTimer extends SceneObjectBase<SceneObjectState> {
+interface LiveNowTimerState extends SceneObjectState {
+  enabled: boolean;
+}
+
+export class LiveNowTimer extends SceneObjectBase<LiveNowTimerState> {
     private timerId: number | undefined = undefined;
     private static REFRESH_RATE = 100; // ms
 
-    public constructor(private enabled = false) {
-      super({});
+    public constructor(enabled = false) {
+      super({ enabled });
       this.addActivationHandler(this._activationHandler);
     }
   
     private _activationHandler = () => {
-      if (this.enabled) {
+      if (this.state.enabled) {
         this.enable();
       }
 
@@ -26,22 +30,22 @@ export class LiveNowTimer extends SceneObjectBase<SceneObjectState> {
     public enable() {
       window.clearInterval(this.timerId);
       this.timerId = undefined;
-      this.enabled = true;
       this.timerId = window.setInterval(() => {
         const panels = sceneGraph.findAllObjects(this.getRoot(), (obj) => obj instanceof VizPanel) as VizPanel[];
         for (const panel of panels) {
           panel.forceRender();
         }
       }, LiveNowTimer.REFRESH_RATE);
+      this.setState({ enabled: true })
     } 
   
     public disable() {
       window.clearInterval(this.timerId);
       this.timerId = undefined;
-      this.enabled = false;
+      this.setState({ enabled: false })
     }
 
     public get isEnabled() {
-        return this.enabled;
+        return this.state.enabled;
     }
 }
