d6b6ec40bc2fa1595e88441b3629dfbc9ae7db2a
Ivan Ortega Alba <ivanortegaalba@gmail.com>
2024-04-02 14:23:48 +0200

    Update packages/scenes/src/components/layout/grid/utils.ts

--- packages/scenes/src/components/layout/grid/utils.ts
+++ packages/scenes/src/components/layout/grid/utils.ts
@@ -8,13 +8,7 @@ export function fitPanelsInHeight(cells: ReactGridLayout.Layout[], height: numbe
   const visibleGridHeight = Math.floor(visibleHeight / (GRID_CELL_HEIGHT + GRID_CELL_VMARGIN));
   const scaleFactor = currentGridHeight / visibleGridHeight;
 
-  console.log('scaleFactor', scaleFactor);
-  console.log('y');
   return cells.map((cell) => {
-    console.log('cell.y', cell.y);
-    console.log('cell.h', cell.h);
-    console.log('Math.round(cell.y / scaleFactor)', Math.round(cell.y / scaleFactor));
-    console.log('Math.round(cell.h / scaleFactor)', Math.round(cell.h / scaleFactor));
     return {
       ...cell,
       y: Math.round(cell.y / scaleFactor) || 0,
