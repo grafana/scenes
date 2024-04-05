e93bb8501d0dfd5fcd77dd19b82fa086afcd0286
Victor Marin <victor.marin@grafana.com>
2024-04-05 16:50:12 +0300

    default to empty panel data is state.data is undefined

--- packages/scenes/src/querying/SceneQueryRunner.ts
+++ packages/scenes/src/querying/SceneQueryRunner.ts
@@ -421,7 +421,7 @@ export class SceneQueryRunner extends SceneObjectBase<QueryRunnerState> implemen
       clone['_layerAnnotations'] = this._layerAnnotations.map((frame) => ({ ...frame }));
     }
 
-    clone['_results'].next({ origin: this, data: this.state.data });
+    clone['_results'].next({ origin: this, data: this.state.data ?? emptyPanelData });
 
     return clone;
   }
