6c009cbdbac97602727452e551732153a960090b
Bogdan Matei <bogdan.matei@grafana.com>
2024-06-07 16:30:50 +0300

    Remove enrichFiltersRequest from SceneApp

--- packages/scenes/src/components/SceneApp/SceneApp.tsx
+++ packages/scenes/src/components/SceneApp/SceneApp.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import { DataRequestEnricher, FiltersRequestEnricher, SceneComponentProps } from '../../core/types';
+import { DataRequestEnricher, SceneComponentProps } from '../../core/types';
 import { SceneObjectBase } from '../../core/SceneObjectBase';
 import { SceneAppState } from './types';
 import { renderSceneComponentWithRouteProps } from './utils';
@@ -9,14 +9,8 @@ import { renderSceneComponentWithRouteProps } from './utils';
 /**
  * Responsible for top level pages routing
  */
-export class SceneApp extends SceneObjectBase<SceneAppState> implements DataRequestEnricher, FiltersRequestEnricher {
+export class SceneApp extends SceneObjectBase<SceneAppState> implements DataRequestEnricher {
   public enrichDataRequest() {
-    return {
-      app: this.state.name || 'app',
-    };
-  }
-
-  public enrichFiltersRequest() {
     return {
       app: this.state.name || 'app',
     };
