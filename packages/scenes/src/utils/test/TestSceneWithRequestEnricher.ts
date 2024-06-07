2469f90ebf3a6f0b39f8fac6aceb738e9f8f8c45
Bogdan Matei <bogdan.matei@grafana.com>
2024-06-07 16:49:45 +0300

    Add tests

--- packages/scenes/src/utils/test/TestSceneWithRequestEnricher.ts
+++ packages/scenes/src/utils/test/TestSceneWithRequestEnricher.ts
@@ -1,15 +1,8 @@
 import { SceneObjectBase } from '../../core/SceneObjectBase';
-import { SceneObjectState, DataRequestEnricher, FiltersRequestEnricher, SceneObject } from '../../core/types';
+import { SceneObjectState, DataRequestEnricher, SceneObject } from '../../core/types';
 
-export class TestSceneWithRequestEnricher
-  extends SceneObjectBase<SceneObjectState>
-  implements DataRequestEnricher, FiltersRequestEnricher
-{
+export class TestSceneWithRequestEnricher extends SceneObjectBase<SceneObjectState> implements DataRequestEnricher {
   public enrichDataRequest(_: SceneObject) {
-    return { app: 'enriched' };
-  }
-
-  public enrichFiltersRequest(_: SceneObject) {
     return { app: 'enriched' };
   }
 }
