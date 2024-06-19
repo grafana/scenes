3579ba3041203778f77931f1cecd522178ffb91a
Sergej-Vlasov <37613182+Sergej-Vlasov@users.noreply.github.com>
2024-06-19 11:07:19 +0300

    VariableValueSelect: Hardcode flaky Select option e2e-selector (#797)
    Co-authored-by: Dominik Prokop <dominik.prokop@grafana.com>

--- packages/scenes/src/variables/components/VariableValueSelect.tsx
+++ packages/scenes/src/variables/components/VariableValueSelect.tsx
@@ -181,7 +181,9 @@ export const OptionWithCheckbox = ({
       ref={innerRef}
       className={cx(selectStyles.option, isFocused && selectStyles.optionFocused)}
       {...rest}
-      data-testid={selectors.components.Select.option}
+      // TODO: use below selector once we update grafana dependencies to ^11.1.0
+      // data-testid={selectors.components.Select.option}
+      data-testid="data-testid Select option"
       title={data.title}
     >
       <div className={optionStyles.checkbox}>
