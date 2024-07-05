d09782ec57ef26dbd6ddfd400bd60d291cf67151
Victor Marin <victor.marin@grafana.com>
2024-07-05 10:43:04 +0300

    fix

--- packages/scenes-react/src/variables/types.ts
+++
@@ -1,9 +0,0 @@
-import { VariableValue } from "@grafana/scenes";
-import { VariableHide } from "@grafana/schema";
-
-export interface VariableProps {
-  name: string,
-  label?: string;
-  hide?: VariableHide;
-  initialValue?: VariableValue;
-}
