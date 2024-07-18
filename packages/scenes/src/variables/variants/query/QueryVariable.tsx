b8511a4866563bc5198408c474bd4b418702ed7a
Ivan Ortega <ivanortegaalba@gmail.com>
2024-07-18 12:32:05 +0200

    Value is not returned when query is empty

--- packages/scenes/src/variables/variants/query/QueryVariable.tsx
+++ packages/scenes/src/variables/variants/query/QueryVariable.tsx
@@ -61,10 +61,6 @@ export class QueryVariable extends MultiValueVariable<QueryVariableState> {
   }
 
   public getValueOptions(args: VariableGetOptionsArgs): Observable<VariableValueOption[]> {
-    if (!this.state.query) {
-      return of([]);
-    }
-
     this.setState({ loading: true, error: null });
 
     return from(
