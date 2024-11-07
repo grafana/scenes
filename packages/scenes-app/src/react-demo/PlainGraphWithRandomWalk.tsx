11cbd2c143b196ac453481d24b460eb1cb376727
Torkel Ödegaard <torkel@grafana.com>
2024-11-07 10:13:29 +0100

    ScenesReact: Cache SceneQueryRunners and other scene object by a key / hashing string (#788)
    Co-authored-by: Victor Marin <victor.marin@grafana.com>

--- packages/scenes-app/src/react-demo/PlainGraphWithRandomWalk.tsx
+++ packages/scenes-app/src/react-demo/PlainGraphWithRandomWalk.tsx
@@ -8,10 +8,13 @@ interface Props {
   queryAlias?: string;
 }
 
-export function PlainGraphWithRandomWalk({ maxDataPoints, title, queryAlias }: Props) {
+export function PlainGraphWithRandomWalk({ maxDataPoints = 20, title, queryAlias }: Props) {
+  const queries = [{ uid: 'gdev-testdata', refId: 'A', scenarioId: 'random_walk', alias: queryAlias ?? 'env = $env' }];
+
   const dataProvider = useQueryRunner({
-    queries: [{ uid: 'gdev-testdata', refId: 'A', scenarioId: 'random_walk', alias: queryAlias ?? 'env = $env' }],
-    maxDataPoints: maxDataPoints ?? 20,
+    queries,
+    maxDataPoints: maxDataPoints,
+    cacheKey: [queries, maxDataPoints],
   });
 
   return <VizPanel title={title} viz={plainGraph} dataProvider={dataProvider} />;
