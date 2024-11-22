f1cf70e444fdd55e3329ca318fb3667275a2d1ea
Galen <galen.kistler@grafana.com>
2024-11-22 06:32:02 -0600

    chore: clean up comment copy

--- packages/scenes/src/core/PanelBuilders/VizPanelBuilder.ts
+++ packages/scenes/src/core/PanelBuilders/VizPanelBuilder.ts
@@ -62,7 +62,7 @@ export class VizPanelBuilder<TOptions extends {}, TFieldConfig extends {}>
   }
 
   /**
-   * Set if VizPanelMenu "kebab" icon is shown on panel hover for desktop devices. Set as false to always show menu icon.
+   * Set if VizPanelMenu "kebab" icon is shown on panel hover for desktop devices. Set true to always show menu icon.
    * @param showMenuAlways
    */
   public setShowMenuAlways(showMenuAlways: VizPanelState['showMenuAlways']): this {
