b38482de3f37d1a861bffc06989fc599502279de
Victor Marin <victor.marin@grafana.com>
2024-11-22 17:16:10 +0200

    wip add adHoc + groupby vars to scenes-react

--- packages/scenes-app/src/react-demo/DynamicVariablesPage.tsx
+++ packages/scenes-app/src/react-demo/DynamicVariablesPage.tsx
@@ -1,10 +1,20 @@
-import { CustomVariable, DataSourceVariable, QueryVariable, VariableControl } from '@grafana/scenes-react';
+import {
+  AdHocFiltersVariable,
+  CustomVariable,
+  DataSourceVariable,
+  GroupByVariable,
+  QueryVariable,
+  VariableControl,
+  VizPanel,
+  useQueryRunner,
+} from '@grafana/scenes-react';
 import { Button, Stack } from '@grafana/ui';
 import React from 'react';
 import { PageWrapper } from './PageWrapper';
 import { PlainGraphWithRandomWalk } from './PlainGraphWithRandomWalk';
 import { DemoVizLayout } from './utils';
 import { VariableHide, VariableRefresh, VariableSort } from '@grafana/schema';
+import { VizConfigBuilders } from '@grafana/scenes';
 
 export function DynamicVariablesPage() {
   const [regexDsVar, setRegexDsVar] = React.useState<string | undefined>(undefined);
@@ -41,12 +51,12 @@ export function DynamicVariablesPage() {
           </DemoVizLayout>
         </Stack>
       </DataSourceVariable>
-      <QueryVariable 
-        name="job2" 
+      <QueryVariable
+        name="job2"
         initialValue="A"
-        query={{ query: '*', refId: 'A' }} 
-        datasource={{ uid: 'gdev-testdata' }} 
-        regex={regexQueryVar} 
+        query={{ query: '*', refId: 'A' }}
+        datasource={{ uid: 'gdev-testdata' }}
+        regex={regexQueryVar}
         hide={hide}
         refresh={VariableRefresh.onTimeRangeChanged}
         includeAll={includeAll}
@@ -74,7 +84,7 @@ export function DynamicVariablesPage() {
               Remove include all
             </Button>
             <Button variant="secondary" onClick={() => setSort(VariableSort.alphabeticalAsc)}>
-              Sort asc
+              Sort asc{' '}
             </Button>
             <Button variant="secondary" onClick={() => setSort(VariableSort.alphabeticalDesc)}>
               Sort desc
@@ -85,6 +95,85 @@ export function DynamicVariablesPage() {
           </DemoVizLayout>
         </Stack>
       </QueryVariable>
+      <GroupByVariable
+        name="groupBy"
+        initialValue="instance"
+        datasource={{ uid: 'gdev-prometheus' }}
+        label="Group by"
+        defaultOptions={[
+          { text: 'Instance', value: 'instance' },
+          { text: 'Job', value: 'job' },
+          { text: 'Cluster', value: 'cluster' },
+        ]}
+      >
+        <Stack direction="column">
+          <Stack>
+            <VariableControl name="groupBy" />
+          </Stack>
+          <DemoVizLayout>
+            <TableWithAggregatedPrometheusData />
+          </DemoVizLayout>
+        </Stack>
+      </GroupByVariable>
+      <AdHocFiltersVariable
+        name="test_var"
+        datasource={{ uid: 'gdev-prometheus' }}
+        label="AdHocFilers var"
+        baseFilters={[{ key: '__name__', operator: '=', value: 'ALERTS', condition: '' }]}
+        supportsMultiValueOperators={true}
+      >
+        <Stack direction="column">
+          <Stack>
+            <VariableControl name="test_var" />
+          </Stack>
+          <DemoVizLayout>
+            <TableWithPrometheusData />
+          </DemoVizLayout>
+        </Stack>
+      </AdHocFiltersVariable>
     </PageWrapper>
   );
+  //These demos should be moved to individual tabs in the future. this is getting cumbersome
+}
+
+export function TableWithAggregatedPrometheusData() {
+  const queries = [
+    {
+      datasource: { uid: 'gdev-prometheus' },
+      refId: 'A',
+      expr: 'count(ALERTS)',
+      format: 'table',
+      instant: true,
+    },
+  ];
+
+  const tableGraph = VizConfigBuilders.table().build();
+  const dataProvider = useQueryRunner({
+    queries,
+    maxDataPoints: 20,
+    cacheKey: [queries],
+  });
+
+  return <VizPanel title={'Table panel'} viz={tableGraph} dataProvider={dataProvider} />;
+}
+
+export function TableWithPrometheusData() {
+  const queries = [
+    {
+      datasource: { uid: 'gdev-prometheus' },
+      refId: 'A',
+      expr: 'ALERTS',
+      format: 'table',
+      instant: true,
+    },
+  ];
+
+  const tableGraph = VizConfigBuilders.table().build();
+  const dataProvider = useQueryRunner({
+    queries,
+    maxDataPoints: 20,
+    cacheKey: [queries],
+  });
+
+  return <VizPanel title={'Table panel'} viz={tableGraph} dataProvider={dataProvider} />;
 }
