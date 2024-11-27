b1b43b285b0936868ab3fc1dfe57f83af1e9a56e
Marc Mignonsin <marc.mignonsin@grafana.com>
2024-11-27 12:36:46 +0100

    fix: Account for null case

--- packages/scenes/src/variables/components/VariableValueSelectors.tsx
+++ packages/scenes/src/variables/components/VariableValueSelectors.tsx
@@ -79,7 +79,7 @@ function VariableLabel({ variable, layout, hideLabel }: VariableSelectProps) {
       isLoading={state.loading}
       onCancel={() => variable.onCancel?.()}
       label={labelOrName}
-      error={typeof state.error === 'object' ? state.error.message : state.error}
+      error={state.error?.message || state.error}
       layout={layout}
       description={state.description ?? undefined}
     />
