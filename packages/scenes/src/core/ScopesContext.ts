82a70e360c457bb1a251530d87028b8916c6f079
Bogdan Matei <bogdan.matei@grafana.com>
2024-12-05 17:58:54 +0200

    Refactor

--- packages/scenes/src/core/ScopesContext.ts
+++ packages/scenes/src/core/ScopesContext.ts
@@ -1,6 +1,6 @@
 import { createContext, useContext } from 'react';
 import { useObservable } from 'react-use';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { Scope } from '@grafana/data';
 
@@ -9,21 +9,14 @@ export interface ScopesContextValue {
     isEnabled: boolean;
     isLoading: boolean;
     isReadOnly: boolean;
-    pendingScopes: string[] | null;
     value: Scope[];
   };
   stateObservable: Observable<ScopesContextValue['state']>;
-  setNewScopes: (scopeNames: string[] | null) => void;
-  setCurrentScopes: (scopes: Scope[]) => void;
-  enterLoadingMode: () => void;
-  exitLoadingMode: () => void;
+  changeScopes: (scopeNames: string[]) => void;
   enterReadOnly: () => void;
   exitReadOnly: () => void;
   enable: () => void;
   disable: () => void;
-  subscribeToState: (
-    cb: (newState: ScopesContextValue['state'], prevState: ScopesContextValue['state']) => void
-  ) => Subscription;
 }
 
 export const ScopesContext = createContext<ScopesContextValue | undefined>(undefined);
@@ -33,9 +26,15 @@ export function useScopes() {
 
   useObservable(context?.stateObservable ?? new Observable(), context?.state);
 
-  if (!context) {
-    return undefined;
-  }
-
-  return context;
+  return context
+    ? {
+        state: context.state,
+        stateObservable: context.stateObservable,
+        changeScopes: context.changeScopes,
+        enterReadOnly: context.enterReadOnly,
+        exitReadOnly: context.exitReadOnly,
+        enable: context.enable,
+        disable: context.disable,
+      }
+    : undefined;
 }
