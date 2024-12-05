f3782a70907751de8a761c8c49b30a15a44bd138
Bogdan Matei <bogdan.matei@grafana.com>
2024-12-05 17:58:54 +0200

    Refactor

--- packages/scenes/src/core/SceneScopesBridge.ts
+++ packages/scenes/src/core/SceneScopesBridge.ts
@@ -1,24 +1,46 @@
 import { isEqual } from 'lodash';
-import { useEffect } from 'react';
 import { BehaviorSubject, filter, map, Observable, pairwise, Unsubscribable } from 'rxjs';
 
 import { Scope } from '@grafana/data';
 
 import { SceneObjectBase } from './SceneObjectBase';
-import { SceneComponentProps, SceneObjectState } from './types';
+import { SceneComponentProps, SceneObjectState, SceneObjectUrlValues, SceneObjectWithUrlSync } from './types';
 import { ScopesContextValue, useScopes } from './ScopesContext';
+import { SceneObjectUrlSyncConfig } from '../services/SceneObjectUrlSyncConfig';
 
 export interface SceneScopesBridgeState extends SceneObjectState {}
 
-export class SceneScopesBridge extends SceneObjectBase<SceneScopesBridgeState> {
+export class SceneScopesBridge extends SceneObjectBase<SceneScopesBridgeState> implements SceneObjectWithUrlSync {
   static Component = SceneScopesBridgeRenderer;
+
+  protected _urlSync = new SceneObjectUrlSyncConfig(this, { keys: ['scopes'] });
 
   protected _renderBeforeActivation = true;
 
   private _contextSubject = new BehaviorSubject<ScopesContextValue | undefined>(undefined);
 
+  private _pendingScopes: string[] | null = null;
+
   public constructor(state: SceneScopesBridgeState) {
     super(state);
+  }
+
+  public getUrlState(): SceneObjectUrlValues {
+    return {
+      scopes: this._pendingScopes ?? (this.context?.state.value ?? []).map((scope) => scope.metadata.name),
+    };
+  }
+
+  public updateFromUrl(values: SceneObjectUrlValues) {
+    let scopes = values['scopes'] ?? [];
+    scopes = (Array.isArray(scopes) ? scopes : [scopes]).map(String);
+
+    if (!this.context) {
+      this._pendingScopes = scopes;
+      return;
+    }
+
+    this.context?.changeScopes(scopes);
   }
 
   public getValue(): Scope[] {
@@ -78,8 +100,21 @@ export class SceneScopesBridge extends SceneObjectBase<SceneScopesBridgeState> {
   }
 
   public updateContext(newContext: ScopesContextValue | undefined) {
-    if (this.context !== newContext) {
+    if (this._pendingScopes && newContext) {
+      setTimeout(() => {
+        newContext?.changeScopes(this._pendingScopes!);
+        this._pendingScopes = null;
+      });
+    }
+
+    if (this.context !== newContext || this.context?.state !== newContext?.state) {
+      const shouldUpdate = this.context?.state.value !== newContext?.state.value;
+
       this._contextSubject.next(newContext);
+
+      if (shouldUpdate) {
+        setTimeout(() => this.forceRender());
+      }
     }
   }
 
@@ -95,9 +130,7 @@ export class SceneScopesBridge extends SceneObjectBase<SceneScopesBridgeState> {
 function SceneScopesBridgeRenderer({ model }: SceneComponentProps<SceneScopesBridge>) {
   const context = useScopes();
 
-  useEffect(() => {
-    model.updateContext(context);
-  }, [context, model]);
+  model.updateContext(context);
 
   return null;
 }
