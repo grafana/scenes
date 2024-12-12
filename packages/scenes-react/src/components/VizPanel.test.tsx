687436f48b01b8654876d0970f38cc95433b4139
Galen <galen.kistler@grafana.com>
2024-12-12 09:13:42 -0600

    test: update unit tests

--- packages/scenes-react/src/components/VizPanel.test.tsx
+++ packages/scenes-react/src/components/VizPanel.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { SceneContextObject } from '../contexts/SceneContextObject';
-import { VizConfigBuilders, VizPanel as VizPanelObject } from '@grafana/scenes';
-import { VizPanel } from './VizPanel';
+import { VizConfigBuilders, VizPanel as VizPanelObject, VizPanelMenu } from '@grafana/scenes';
+import { VizPanel, VizPanelProps } from './VizPanel';
 import { PanelPlugin } from '@grafana/data';
 import { TestContextProvider } from '../utils/testUtils';
+import { PanelChrome } from '@grafana/ui';
 
 let pluginToLoad: PanelPlugin | undefined;
 
@@ -61,13 +62,26 @@ describe('VizPanel', () => {
     const viz = VizConfigBuilders.timeseries().build();
     const titleItems = <div>Title Item</div>;
 
-    const { getByText } = render(
+    const {rerender, unmount} = render(
       <TestContextProvider value={scene}>
-        <VizPanel title="Test" titleItems={titleItems} viz={viz} />
+        <VizPanel titleItems={titleItems} title="Test" viz={viz} />
       </TestContextProvider>
     );
 
-    expect(getByText('Title Item')).toBeInTheDocument();
+    const panel = scene.state.children[0] as VizPanelObject;
+    expect(panel.state.titleItems).toEqual(titleItems)
+
+    rerender(
+      <TestContextProvider value={scene}>
+        <VizPanel titleItems={undefined} title="Test" viz={viz} />
+      </TestContextProvider>
+    )
+
+    expect(panel.state.titleItems).toEqual(undefined)
+
+    unmount()
+
+    expect(scene.state.children.length).toBe(0);
   });
 
   it('Should render with headerActions', () => {
@@ -75,12 +89,80 @@ describe('VizPanel', () => {
     const viz = VizConfigBuilders.timeseries().build();
     const headerActions = <button>Action</button>;
 
-    const { getByText } = render(
+    const { rerender, unmount } = render(
       <TestContextProvider value={scene}>
-        <VizPanel title="Test" headerActions={headerActions} viz={viz} />
+        <VizPanel title="Test" viz={viz} headerActions={headerActions} />
       </TestContextProvider>
     );
 
-    expect(getByText('Action')).toBeInTheDocument();
+    const panel = scene.state.children[0] as VizPanelObject;
+    expect(panel.state.headerActions).toEqual(headerActions)
+
+    rerender(
+      <TestContextProvider value={scene}>
+        <VizPanel title="Test" viz={viz} />
+      </TestContextProvider>
+    )
+
+    unmount()
+
+    expect(scene.state.children.length).toBe(0);
+  });
+
+  it('Should render VizPanelProps', () => {
+    const scene = new SceneContextObject();
+    const viz = VizConfigBuilders.timeseries().build();
+    const headerActions = <button>Action</button>;
+    const seriesLimit = 1;
+    const collapsed = true
+    const collapsible = true;
+    const hoverHeader = true;
+    const description = 'description'
+    const menu = new VizPanelMenu({})
+    const title = 'title'
+    const props: VizPanelProps = {
+      title,
+      viz,
+      headerActions,
+      seriesLimit,
+      collapsed,
+      collapsible,
+      hoverHeader,
+      description,
+      menu
+    }
+
+    const { rerender, unmount } = render(
+      <TestContextProvider value={scene}>
+        <VizPanel {...props} headerActions={headerActions} />
+      </TestContextProvider>
+    );
+
+    const panel = scene.state.children[0] as VizPanelObject;
+    expect(panel.state.headerActions).toEqual(headerActions)
+    expect(panel.state.collapsed).toEqual(collapsed)
+    expect(panel.state.seriesLimit).toEqual(seriesLimit)
+    expect(panel.state.collapsible).toEqual(collapsible)
+    expect(panel.state.hoverHeader).toEqual(hoverHeader)
+    expect(panel.state.description).toEqual(description)
+    expect(panel.state.menu).toEqual(menu)
+
+    rerender(
+      <TestContextProvider value={scene}>
+        <VizPanel title="Test" viz={viz} />
+      </TestContextProvider>
+    )
+
+    expect(panel.state.headerActions).toEqual(undefined)
+    expect(panel.state.collapsed).toEqual(undefined)
+    expect(panel.state.seriesLimit).toEqual(undefined)
+    expect(panel.state.collapsible).toEqual(undefined)
+    expect(panel.state.hoverHeader).toEqual(undefined)
+    expect(panel.state.description).toEqual(undefined)
+    expect(panel.state.menu).toEqual(undefined)
+
+    unmount()
+
+    expect(scene.state.children.length).toBe(0);
   });
 });
