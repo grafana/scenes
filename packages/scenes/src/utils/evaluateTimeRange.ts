c21c130c6ab0a5d2d531e9422370b2eaa6df33b3
oscarkilhed <oscar.kilhed@grafana.com>
2024-12-12 16:21:55 +0100

    Fix comments

--- packages/scenes/src/utils/evaluateTimeRange.ts
+++ packages/scenes/src/utils/evaluateTimeRange.ts
@@ -12,7 +12,7 @@ export function evaluateTimeRange(
   const now = Date.now();
 
   /** This tries to use dateMath.toDateTime if available, otherwise falls back to dateMath.parse.
-   * Using dateMath.parse can potentially result in now being calculated using two different timestamps.
+   * Using dateMath.parse can potentially result in to and from being calculated using two different timestamps.
    * If two different timestamps are used, the time range "now-24h to now" will potentially be 24h +- number of milliseconds it takes between calculations.
    */
   const parseOrToDateTime = (
@@ -28,7 +28,7 @@ export function evaluateTimeRange(
 
   /** The order of calculating to and from is important. This is because if we're using the old dateMath.parse we could potentially get two different timestamps.
    * If we calculate to first, then from. The timerange "now-24h to now" will err on the side of being shorter than 24h. This will aleviate some of the issues arising
-   * the timerange indeterminently alternating between less than or equal to 24h and being greater than 24h.
+   * from the timerange indeterminently alternating between less than or equal to 24h and being greater than 24h.
    */
   return {
     to: parseOrToDateTime(hasDelay ? 'now-' + delay : to, {
