4b600f9bc4bbd7fa673844cbc127354aa792beb3
Jack Westbrook <jack.westbrook@gmail.com>
2025-04-01 12:30:16 +0200

    Bump rollup and related deps (#1077)

--- packages/scenes-react/rollup.config.ts
+++ packages/scenes-react/rollup.config.ts
@@ -1,16 +1,22 @@
+import { createRequire } from 'node:module';
 import resolve from '@rollup/plugin-node-resolve';
 import path from 'path';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 import eslint from '@rollup/plugin-eslint';
-import { externals } from 'rollup-plugin-node-externals';
+import { nodeExternals } from 'rollup-plugin-node-externals';
 const env = process.env.NODE_ENV || 'production';
-const pkg = require('./package.json');
+const rq = createRequire(import.meta.url);
+
+const pkg = rq('./package.json');
 
 const plugins = [
-  externals({ deps: true, devDeps: true, packagePath: './package.json' }),
+  nodeExternals({ deps: true, devDeps: true, packagePath: './package.json' }),
   resolve({ browser: true }),
-  esbuild(),
+  esbuild({
+    target: 'es2018',
+    tsconfig: './tsconfig.json',
+  }),
   eslint(),
 ];
 
@@ -23,6 +29,8 @@ export default [
         format: 'cjs',
         sourcemap: env === 'production' ? true : 'inline',
         dir: path.dirname(pkg.main),
+        esModule: true,
+        interop: 'compat',
       },
       {
         format: 'esm',
