1edd0a4c790230d50e0daf06f64be387fece9380
Scott Lepper <scott.lepper@gmail.com>
2025-04-24 16:12:56 -0400

    Use log util.  Direct console.warn fails tests in grafana. (#1110)

--- packages/scenes/src/core/SceneTimeRange.tsx
+++ packages/scenes/src/core/SceneTimeRange.tsx
@@ -11,6 +11,7 @@ import { evaluateTimeRange } from '../utils/evaluateTimeRange';
 import { config, locationService, RefreshEvent } from '@grafana/runtime';
 import { isValid } from '../utils/date';
 import { getQueryController } from './sceneGraph/getQueryController';
+import { writeSceneLog } from '../utils/writeSceneLog';
 
 export class SceneTimeRange extends SceneObjectBase<SceneTimeRangeState> implements SceneTimeRangeLike {
   protected _urlSync = new SceneObjectUrlSyncConfig(this, { keys: ['from', 'to', 'timezone', 'time', 'time.window'] });
@@ -272,12 +273,15 @@ function getTimeWindow(time: string, timeWindow: string) {
  * @returns {string | undefined} - Returns the input time zone if it is valid, or undefined if the input is invalid or not provided.
  */
 function getValidTimeZone(timeZone?: string): string | undefined {
-  if (timeZone === undefined || timeZone === defaultTimeZone) {
+  if (timeZone === undefined || timeZone === '') {
+    return undefined;
+  }
+  if (timeZone === defaultTimeZone) {
     return timeZone;
   }
   if (getZone(timeZone)) {
     return timeZone;
   }
-  console.warn(`Invalid timeZone "${timeZone}" provided.`);
+  writeSceneLog('SceneTimeRange', `Invalid timeZone "${timeZone}" provided.`);
   return;
 }
