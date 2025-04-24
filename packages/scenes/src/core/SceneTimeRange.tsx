29b72d3f171166e520006964643e80708e56b576
Scott Lepper <scott.lepper@gmail.com>
2025-04-24 15:14:31 -0400

    handle empty string

--- packages/scenes/src/core/SceneTimeRange.tsx
+++ packages/scenes/src/core/SceneTimeRange.tsx
@@ -12,6 +12,8 @@ import { config, locationService, RefreshEvent } from '@grafana/runtime';
 import { isValid } from '../utils/date';
 import { getQueryController } from './sceneGraph/getQueryController';
 import { writeSceneLog } from '../utils/writeSceneLog';
+import { time } from 'console';
+import { isEmpty } from 'lodash';
 
 export class SceneTimeRange extends SceneObjectBase<SceneTimeRangeState> implements SceneTimeRangeLike {
   protected _urlSync = new SceneObjectUrlSyncConfig(this, { keys: ['from', 'to', 'timezone', 'time', 'time.window'] });
@@ -273,10 +275,10 @@ function getTimeWindow(time: string, timeWindow: string) {
  * @returns {string | undefined} - Returns the input time zone if it is valid, or undefined if the input is invalid or not provided.
  */
 function getValidTimeZone(timeZone?: string): string | undefined {
-  if (timeZone === undefined || timeZone === defaultTimeZone) {
+  if (isEmpty(timeZone) || timeZone === defaultTimeZone) {
     return timeZone;
   }
-  if (getZone(timeZone)) {
+  if (getZone(timeZone!)) {
     return timeZone;
   }
   writeSceneLog('SceneTimeRange', `Invalid timeZone "${timeZone}" provided.`);
