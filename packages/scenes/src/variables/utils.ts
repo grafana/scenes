cfe5905abb57b623820b72cae5a58cd8f4e9676e
Galen <galen.kistler@grafana.com>
2025-05-28 15:25:56 -0500

    chore: clean up

--- packages/scenes/src/variables/utils.ts
+++ packages/scenes/src/variables/utils.ts
@@ -193,7 +193,7 @@ export function escapeInjectedFilterUrlDelimiters(value: string | undefined): st
 }
 
 export function escapeURLDelimiters(value: string | undefined): string {
-  return escapeUrlCommaDelimiters(escapeUrlPipeDelimiters(escapeUrlHashDelimiters(value)));
+  return escapeUrlCommaDelimiters(escapeUrlPipeDelimiters(value));
 }
 
 export function unescapeUrlDelimiters(value: string | undefined): string {
