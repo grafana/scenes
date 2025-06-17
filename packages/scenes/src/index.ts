f044e8e6a12df6f6338d21ecf9a4f4bfa7b132f0
joshhunt <josh.hunt@grafana.com>
2025-06-17 15:15:00 +0100

    rename loadResources

--- packages/scenes/src/index.ts
+++ packages/scenes/src/index.ts
@@ -164,4 +164,4 @@ export const sceneUtils = {
 
 export { SafeSerializableSceneObject } from './utils/SafeSerializableSceneObject';
 export { getExploreURL } from './utils/explore';
-export { initTranslations } from './utils/loadTranslationResources';
+export { loadResources } from './utils/loadResources';
--- packages/scenes/src/utils/loadResources.ts
+++ packages/scenes/src/utils/loadResources.ts
@@ -1,15 +1,14 @@
-import { LANGUAGES, ResourceLoader, initPluginTranslations, type Resources } from '@grafana/i18n';
+import { LANGUAGES, ResourceLoader, Resources } from '@grafana/i18n';
 
 const resources = LANGUAGES.reduce<Record<string, () => Promise<{ default: Resources }>>>((acc, lang) => {
   acc[lang.code] = async () => await import(`../locales/${lang.code}/grafana-scenes.json`);
   return acc;
 }, {});
 
+/**
+ * Loads the translation resources for the given language and returns them.
+ */
 export const loadResources: ResourceLoader = async (resolvedLanguage: string) => {
   const translation = await resources[resolvedLanguage]();
   return translation.default;
-};
-
-export const initTranslations = () => {
-  return initPluginTranslations('grafana-scenes', [loadResources]);
 };
