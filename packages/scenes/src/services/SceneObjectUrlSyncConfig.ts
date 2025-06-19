ba23edaef00af3b51974bba8521649fe3e81f85d
Marc Mignonsin <marc.mignonsin@grafana.com>
2025-06-19 15:39:39 +0200

    revert: Revert changes to SceneObjectUrlSyncConfig

--- packages/scenes/src/services/SceneObjectUrlSyncConfig.ts
+++ packages/scenes/src/services/SceneObjectUrlSyncConfig.ts
@@ -2,36 +2,19 @@ import { SceneObjectUrlSyncHandler, SceneObjectWithUrlSync, SceneObjectUrlValues
 
 interface SceneObjectUrlSyncConfigOptions {
   keys: string[] | (() => string[]);
-  namespace?: string;
-  excludeFromNamespace?: string[];
 }
 
 export class SceneObjectUrlSyncConfig implements SceneObjectUrlSyncHandler {
   private _keys: string[] | (() => string[]);
-  private _namespace: string;
-  private _excludeFromNamespace: string[];
   private _nextChangeShouldAddHistoryStep = false;
 
   public constructor(private _sceneObject: SceneObjectWithUrlSync, _options: SceneObjectUrlSyncConfigOptions) {
     this._keys = _options.keys;
-    this._namespace = _options.namespace ?? '';
-    this._excludeFromNamespace = _options.excludeFromNamespace ?? [];
-
-    if (this._namespace && typeof this._keys !== 'function') {
-      this._keys = this._keys.map((key) => this._getNamespacedKey(key));
-    }
-  }
-
-  private _getNamespacedKey(keyWithoutNamespace: string) {
-    if (this._namespace && !this._excludeFromNamespace.includes(keyWithoutNamespace)) {
-      return `${this._namespace}-${keyWithoutNamespace}`;
-    }
-    return keyWithoutNamespace;
   }
 
   public getKeys(): string[] {
     if (typeof this._keys === 'function') {
-      return this._keys().map((key) => this._getNamespacedKey(key));
+      return this._keys();
     }
 
     return this._keys;
