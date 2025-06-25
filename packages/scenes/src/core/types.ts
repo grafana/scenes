892534e28a3ead381e78e81450e5ae5e69c3b9a9
Torkel Ödegaard <torkel@grafana.com>
2025-06-25 15:05:14 +0200

    sceneGraph: findObject should return first match

--- packages/scenes/src/core/types.ts
+++ packages/scenes/src/core/types.ts
@@ -120,8 +120,9 @@ export interface SceneObject<TState extends SceneObjectState = SceneObjectState>
   /**
    * Loop through state and call callback for each direct child scene object.
    * Checks 1 level deep properties and arrays. So a scene object hidden in a nested plain object will not be detected.
+   * Return false to exit loop early.
    */
-  forEachChild(callback: (child: SceneObject) => void): void;
+  forEachChild(callback: (child: SceneObject) => void): void | false;
 
   /**
    * Useful for edge cases when you want to move a scene object to another parent.
