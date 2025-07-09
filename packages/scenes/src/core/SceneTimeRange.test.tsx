5766836f028c120d9a05f476a0e558fb3670686b
Haris Rozajac <haris.rozajac12@gmail.com>
2025-07-09 17:18:39 -0600

    add test

--- packages/scenes/src/core/SceneTimeRange.test.tsx
+++ packages/scenes/src/core/SceneTimeRange.test.tsx
@@ -160,6 +160,13 @@ describe('SceneTimeRange', () => {
         expect(innerTimeRange.getTimeZone()).toEqual(outerTimeRange.getTimeZone());
       });
     });
+    describe('when user selects default time zone', () => {
+      it(`should return default time zone set in user profile settings`, () => {
+        const timeRange = new SceneTimeRange({ from: 'now-1h', to: 'now' });
+        timeRange.onTimeZoneChange('');
+        expect(timeRange.getTimeZone()).toBe(USER_PROFILE_DEFAULT_TIME_ZONE);
+      });
+    });
     describe('when time zone is specified', () => {
       it('should return own time zone', () => {
         const timeRange = new SceneTimeRange({ from: 'now-1h', to: 'now', timeZone: 'America/New_York' });
