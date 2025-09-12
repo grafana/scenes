b83b4655b4cc6b95e9727271fa8f3e2138ce7465
Marc Mignonsin <marc.mignonsin@grafana.com>
2025-09-12 14:59:19 +0200

    refactor: Introduce CustomOptionsProviders

---
+++ packages/scenes/src/variables/variants/CustomOptionsProviders.ts
@@ -0,0 +1,112 @@
+import { Observable } from 'rxjs';
+import { VariableValueOption } from '../types';
+
+export const CustomOptionsProviders = {
+  fromCsv: (params: CsvProviderParams) => new CsvOptionsProvider(params),
+  fromJson: (params: JsonProviderParams) => new JsonOptionsProvider(params),
+};
+
+interface CustomOptionsProvider {
+  getOptions(): Observable<VariableValueOption[]>;
+}
+
+interface CsvProviderParams {
+  /**
+   * String containing JSON with an array of objects
+   */
+  csv: string;
+}
+
+export class CsvOptionsProvider implements CustomOptionsProvider {
+  public constructor(private params: CsvProviderParams) {}
+
+  public getOptions(): Observable<VariableValueOption[]> {
+    return new Observable((subscriber) => {
+      try {
+        const match = this.params.csv.match(/(?:\\,|[^,])+/g) ?? [];
+
+        const options = match.map((text) => {
+          text = text.replace(/\\,/g, ',');
+          const textMatch = /^\s*(.+)\s:\s(.+)$/g.exec(text) ?? [];
+          if (textMatch.length === 3) {
+            const [, key, value] = textMatch;
+            return { label: key.trim(), value: value.trim() };
+          } else {
+            return { label: text.trim(), value: text.trim() };
+          }
+        });
+
+        subscriber.next(options);
+        subscriber.complete();
+      } catch (error) {
+        subscriber.error(error);
+      }
+    });
+  }
+}
+
+interface JsonProviderParams {
+  /**
+   * String containing JSON with an array of objects
+   */
+  json: string;
+  /**
+   *
+   */
+  valueProp?: string;
+  /**
+   *
+   */
+  textProp?: string;
+}
+
+export class JsonOptionsProvider implements CustomOptionsProvider {
+  public constructor(private params: JsonProviderParams) {}
+
+  private parseAndValidateJson(json: string): VariableValueOption[] {
+    const parsedOptions = JSON.parse(json);
+
+    if (!Array.isArray(parsedOptions)) {
+      throw new Error('Query must be a JSON array');
+    }
+
+    let options = [];
+
+    if (typeof parsedOptions[0] === 'string') {
+      options = parsedOptions.map((value) => ({ label: value.trim(), value: value.trim() }));
+    } else if (typeof parsedOptions[0] === 'object' && parsedOptions[0] !== null) {
+      const { valueProp, textProp } = this.params;
+
+      if (!valueProp) {
+        throw new Error('valueProp must be set');
+      }
+
+      for (const o of parsedOptions as Array<Record<string, any>>) {
+        if (o[valueProp] == null) {
+          continue;
+        }
+
+        options.push({
+          value: o[valueProp].trim(),
+          label: o[textProp as any]?.trim(),
+          properties: o,
+        });
+      }
+    } else {
+      throw new Error('Query must be a JSON array of strings or objects');
+    }
+
+    return options;
+  }
+
+  public getOptions(): Observable<VariableValueOption[]> {
+    return new Observable((subscriber) => {
+      try {
+        subscriber.next(this.parseAndValidateJson(this.params.json));
+        subscriber.complete();
+      } catch (error) {
+        subscriber.error(error);
+      }
+    });
+  }
+}
--- packages/scenes/src/variables/variants/CustomVariable.tsx
+++ packages/scenes/src/variables/variants/CustomVariable.tsx
@@ -1,4 +1,4 @@
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { SceneComponentProps } from '../../core/types';
 import { VariableDependencyConfig } from '../VariableDependencyConfig';
@@ -8,6 +8,7 @@ import { VariableValueOption } from '../types';
 import React from 'react';
 import { sceneGraph } from '../../core/sceneGraph';
 import { MultiValueVariable, MultiValueVariableState, VariableGetOptionsArgs } from './MultiValueVariable';
+import { CustomOptionsProviders } from './CustomOptionsProviders';
 
 export interface CustomVariableState extends MultiValueVariableState {
   query: string;
@@ -35,56 +36,34 @@ export class CustomVariable extends MultiValueVariable<CustomVariableState> {
   public getValueOptions(args: VariableGetOptionsArgs): Observable<VariableValueOption[]> {
     const interpolated = sceneGraph.interpolate(this, this.state.query);
 
-    let options: VariableValueOption[] = [];
+    let optionsProvider;
 
-    if (this.state.isJson) {
-      try {
-        const parsedOptions = JSON.parse(interpolated);
-
-        if (!Array.isArray(parsedOptions)) {
-          throw new Error('Query must be a JSON array');
-        }
-
-        if (typeof parsedOptions[0] === 'string') {
-          options = parsedOptions.map((value) => ({ label: value.trim(), value: value.trim() }));
-        } else if (typeof parsedOptions[0] === 'object' && parsedOptions[0] !== null) {
-          const { valueProp, textProp } = this.state;
-
-          if (!valueProp) {
-            throw new Error('valueProp must be set');
-          }
-
-          options = (parsedOptions as Array<Record<string, any>>).map((o) => ({
-            label: o[valueProp]?.trim(),
-            value: o[textProp as any]?.trim(),
-            properties: o,
-          }));
-        } else {
-          throw new Error('Query must be a JSON array of strings or objects');
-        }
-      } catch (error) {
-        throw error;
-      }
+    if (!this.state.isJson) {
+      optionsProvider = CustomOptionsProviders.fromCsv({ csv: interpolated });
     } else {
-      const match = interpolated.match(/(?:\\,|[^,])+/g) ?? [];
-
-      options = match.map((text) => {
-        text = text.replace(/\\,/g, ',');
-        const textMatch = /^\s*(.+)\s:\s(.+)$/g.exec(text) ?? [];
-        if (textMatch.length === 3) {
-          const [, key, value] = textMatch;
-          return { label: key.trim(), value: value.trim() };
-        } else {
-          return { label: text.trim(), value: text.trim() };
-        }
+      optionsProvider = CustomOptionsProviders.fromJson({
+        json: interpolated,
+        valueProp: this.state.valueProp,
+        textProp: this.state.textProp,
       });
     }
 
-    if (!options.length) {
-      this.skipNextValidation = true;
-    }
-
-    return of(options);
+    return new Observable((subscriber) => {
+      optionsProvider.getOptions().subscribe({
+        next: (options) => {
+          if (!options.length) {
+            this.skipNextValidation = true;
+          }
+          subscriber.next(options);
+        },
+        error: (error) => {
+          subscriber.error(error);
+        },
+        complete: () => {
+          subscriber.complete();
+        },
+      });
+    });
   }
 
   public static Component = ({ model }: SceneComponentProps<MultiValueVariable>) => {
