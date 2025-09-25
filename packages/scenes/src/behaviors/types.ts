b7729b5c7f0a55b40ac9fb20affd3555861d7a09
Dominik Prokop <dominik.prokop@grafana.com>
2025-09-25 15:37:57 +0200

    Remove duplicate SceneInteractionProfileEvent interface
    - Remove SceneInteractionProfileEvent from behaviors/types.ts
    - Interface now only exists locally in grafana DashboardProfiler.ts
    - Simplifies interface duplication and maintains clean API surface

--- packages/scenes/src/behaviors/types.ts
+++ packages/scenes/src/behaviors/types.ts
@@ -18,22 +18,6 @@ export type SceneQueryControllerEntryType = 'data' | 'annotations' | 'variable' 
 // Long Frame Detection types (re-exported from LongFrameDetector)
 export type { LongFrameEvent, LongFrameCallback } from './LongFrameDetector';
 
-// TODO: Remove this interface
-export interface SceneInteractionProfileEvent {
-  origin: string;
-  duration: number;
-  networkDuration: number;
-  jsHeapSizeLimit: number;
-  usedJSHeapSize: number;
-  totalJSHeapSize: number;
-  crumbs: string[];
-  startTs: number;
-  endTs: number;
-  longFramesCount: number;
-  longFramesTotalTime: number;
-  // add more granular data,i.e. network times? slow frames?
-}
-
 export interface SceneComponentInteractionEvent {
   origin: string;
   duration: number;
