3efab229782c08a015a4a4c982fded2043a2969c
Dominik Prokop <dominik.prokop@grafana.com>
2025-10-21 09:28:25 +0200

    Prettier

--- packages/scenes/src/performance/types.ts
+++ packages/scenes/src/performance/types.ts
@@ -4,7 +4,11 @@
 
 export type QueryCompletionCallback = (endTimestamp: number, error?: any) => void;
 export type PluginLoadCompletionCallback = (plugin: any, fromCache?: boolean) => void;
-export type FieldConfigCompletionCallback = (endTimestamp: number, dataPointsCount?: number, seriesCount?: number) => void;
+export type FieldConfigCompletionCallback = (
+  endTimestamp: number,
+  dataPointsCount?: number,
+  seriesCount?: number
+) => void;
 export type RenderCompletionCallback = (endTimestamp: number, duration: number) => void;
 export type DataTransformCompletionCallback = (
   endTimestamp: number,
