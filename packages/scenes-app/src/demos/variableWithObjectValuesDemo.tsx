a95ffc2209105c282dbc7cb1add60247fdb17db6
grafakus <marc.mignonsin@grafana.com>
2026-01-14 11:33:11 +0100

    Update demo

--- packages/scenes-app/src/demos/variableWithObjectValuesDemo.tsx
+++ packages/scenes-app/src/demos/variableWithObjectValuesDemo.tsx
@@ -45,7 +45,6 @@ export function getVariableWithObjectValuesDemo(defaults: SceneAppPageState) {
                   'content',
                   `
 - Current value = \${envs}
-- Value formatter = \${envs:value}
 - Text formatter = \${envs:text}
 `
                 )
@@ -58,7 +57,6 @@ export function getVariableWithObjectValuesDemo(defaults: SceneAppPageState) {
                   'content',
                   `
 - Current value = \${envs.aws_environment}
-- Value formatter = \${envs.aws_environment:value}
 - Text formatter = \${envs.aws_environment:text}
 `
                 )
@@ -71,7 +69,6 @@ export function getVariableWithObjectValuesDemo(defaults: SceneAppPageState) {
                   'content',
                   `
 - Current value = \${envs.azure_environment}
-- Value formatter = \${envs.azure_environment:value}
 - Text formatter = \${envs.azure_environment:text}
 `
                 )
