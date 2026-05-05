d355abfa3be881cc975532f0ffd75f0d6e032522
Kristina Demeshchik <kristina.demeshchik@gmail.com>
2026-05-05 11:19:29 -0400

    AdhocCombobox: apply filter and move focus to the next focusable element on tab (#1435)

--- packages/scenes/src/variables/adhoc/AdHocFiltersCombobox/useFreeFormExpression.ts
+++ packages/scenes/src/variables/adhoc/AdHocFiltersCombobox/useFreeFormExpression.ts
@@ -50,6 +50,7 @@ export function useFreeFormExpression({
 
   const parsedExpression = useMemo(() => parseExpression(inputValue), [parseExpression, inputValue]);
   const canCommitExpressionUpdate = parsedExpression !== null;
+  const canCommitFullExpression = Boolean(parsedExpression?.value);
 
   const commitExpressionUpdate = useCallback((): Partial<AdHocFilterWithLabels> | null => {
     if (!parsedExpression || !filter) {
@@ -92,6 +93,7 @@ export function useFreeFormExpression({
   return {
     parseExpression,
     canCommitExpressionUpdate,
+    canCommitFullExpression,
     commitExpressionUpdate,
   };
 }
